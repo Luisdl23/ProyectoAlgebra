4db4c107cb3a04d38a389daca85c4ea2310cef32
user <quique23@umes.edu.gt>
2024-05-11 12:12:33 -0600

    Cambios para tener carnet

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,30 +30,66 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.labelcarnet = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(317, 56);
+            this.textBox1.Location = new System.Drawing.Point(441, 85);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(226, 22);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 59);
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(215, 16);
+            this.label1.Size = new System.Drawing.Size(368, 27);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingrese su numero de carnet actual";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelcarnet
+            // 
+            this.labelcarnet.AutoSize = true;
+            this.labelcarnet.Location = new System.Drawing.Point(438, 157);
+            this.labelcarnet.Name = "labelcarnet";
+            this.labelcarnet.Size = new System.Drawing.Size(44, 16);
+            this.labelcarnet.TabIndex = 2;
+            this.labelcarnet.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generar nuevo carnet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(616, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generar QR";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelcarnet);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,6 +103,9 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelcarnet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ProyectoAlgebra
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
         public Form1()
         {
+
             InitializeComponent();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numerosAleatorios = new int[4];
+
+            
+            for (int i = 0; i < 4; i++)
+            {
+                numerosAleatorios[i] = random.Next(10) + 1;
+            }
+
+            string textoGuardado = textBox1.Text;
+            textBox1.Select(4, 2);           
+            string textoSeleccionado = textBox1.SelectedText;
+            string carnetnuevo = "2024"+ textoSeleccionado + numerosAleatorios[0] + numerosAleatorios[1] + numerosAleatorios[2];
+         
+            labelcarnet.Text = carnetnuevo;
+
+
+          
+            
+
+            
         }
     }
 }
